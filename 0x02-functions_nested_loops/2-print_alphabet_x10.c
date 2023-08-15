41164afacd882b7178bbb4f342c7a2cd13ab3e6b
abdulrahmanashraf145 <me@example.com>
2023-08-15 16:37:21 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	for (letter = 'a' ; lettr <= 'z' ; letter++)
+		_putchar(letter);
+	_putchar('\n');
+	return(0);
+}
+
