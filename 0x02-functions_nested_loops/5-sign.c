6fa715c6cca78f0e4953fb19f7c80025cec40e3e
abdulrahmanashraf145 <me@example.com>
2023-08-15 17:59:35 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+int print_sign(int n)
+{
+	if (n > 0 )
+	{
+		return 1;
+		printf("+");
+	}
+	else if (n == 0)
+	{
+		return 0;
+		printf("0");
+	}
+	else 
+	{
+		return -1;
+		printf("-");
+	}
+}
